392de6cf95791e8b556f696b662410decb220958
contact@bittich.be <>
2013-03-18 01:24:44 +0100

    Client ajouté et fonctionnel, il faut encore ajouter la partie administrative

---
+++ src/Bittich/HotelBundle/Form/ClientType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bittich\HotelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Bittich\HotelBundle\Form\UserType;
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom','text',
+                    array('label'=>'client.nom',
+                    'required'=>true,))
+            ->add('prenom','text',
+                    array('label'=>'client.prenom',
+                    'required'=>true,))
+            ->add('adresse','text',
+                    array('label'=>'client.adresse',
+                    'required'=>true,))
+            ->add('npostal','text',
+                    array('label'=>'client.npostal',
+                    'required'=>true,))
+            ->add('localite','text',
+                    array('label'=>'client.localite',
+                    'required'=>true,))
+            #ici on fait appel au formulaire fos_userbundle pour créer l'utilisateur
+            ->add('user',new UserType())
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Bittich\HotelBundle\Entity\Client'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'bittich_hotelbundle_clienttype';
+    }
+}
---
+++ src/Bittich/HotelBundle/Tests/Controller/ClientControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bittich\HotelBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ClientControllerTest extends WebTestCase
+{
+}
